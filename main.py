f301de7d64930c35e2d9f09fe0cb951001650a33
ManonVessiot <manon.vessiot@hotmail.com>
2020-09-21 08:56:45 +0200

    add threading

--- main.py
+++ main.py
@@ -3,78 +3,67 @@ import time
 import sys
 import os
 from pynput import keyboard
+import threading
 
 from Movement import Movement
 from SnakeGame import SnakeGame
 
-def trying():
-
-    # The event listener will be running in this block
-    with keyboard.Events() as events:
-        # Block at most one second
-        event = events.get(1.0)
-        if event is None:
-            print('You did not press a key within one second')
-        else:
-            print('Received event {}'.format(event))
-
-
-def main():
+def playInConsole():
+    global width
+    global height
+    global secs
     global move
-
-    width = 20
-    height = 10
-    secs = 0.25
+    global playing
 
     game = SnakeGame(width, height)
     game.draw(True)
 
-    while boucle(game):
-        #time.sleep(secs)
-        timeStart = time.time()
-        timeToPass = secs
-        while timeToPass > 0:
-            # The event listener will be running in this block
-            with keyboard.Events() as events:
-                # Block at most one second
-                event = events.get(secs)
-                if event is not None and isinstance(event, keyboard.Events.Press):
-                    if event.key == keyboard.Key.left:
-                        move = Movement.LEFT
-                    elif event.key == keyboard.Key.up:
-                        move = Movement.UP
-                    elif event.key == keyboard.Key.right:
-                        move = Movement.RIGHT
-                    elif event.key == keyboard.Key.down:
-                        move = Movement.DOWN
+    while move == Movement.NONE:
+        time.sleep(secs)
 
-            timePassed = time.time() - timeStart
-            timeToPass -= timePassed
+    playing = True
+    while playing:
+        time.sleep(secs)
+        playing = playingTurn(game)
+        game.draw(playing)
 
-def boucle(game):
+def playingTurn(game):
     global move
-#    value = input("Move:\n")
-#    move = Movement.NONE
-#    if value == "l":
-#        move = Movement.LEFT
-#    elif value == "r":
-#        move = Movement.RIGHT
-#    elif value == "u":
-#        move = Movement.UP
-#    elif value == "d":
-#        move = Movement.DOWN
-#
+    
     playing = game.moveSnake(move)
-    game.draw(playing)
     return playing
 
-#move = Movement.NONE
-#main()
+def moveEvents():
+    global move
+    global playing
+
+    while playing:
+        # The event listener will be running in this block
+        with keyboard.Events() as events:
+            # Block at most one second
+            event = events.get()
+            if event is not None and isinstance(event, keyboard.Events.Press):
+                if event.key == keyboard.Key.left:
+                    move = Movement.LEFT
+                elif event.key == keyboard.Key.up:
+                    move = Movement.UP
+                elif event.key == keyboard.Key.right:
+                    move = Movement.RIGHT
+                elif event.key == keyboard.Key.down:
+                    move = Movement.DOWN
+
+width = 20
+height = 10
+secs = 0.25
+move = Movement.NONE
+playing = True
 
 if __name__ == '__main__':
     try:
-        move = Movement.NONE
-        main()
+        x = threading.Thread(target=moveEvents, args=())
+        x.start()
+        
+        playInConsole()
     except KeyboardInterrupt:
         print('\nInterrupted')
         try:
